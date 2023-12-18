326789d1338e13c54842e179c0073e830e0c94ea
Dowak1n <leha.kamyak@mail.ru>
2023-12-18 06:09:40 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <chat-bot />
+  </div>
+</template>
+
+<script>
+import ChatBot from 'dowakin-chat-bot';
+
+export default {
+  components: {
+    ChatBot
+  }
+}
+</script>
